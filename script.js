0d92dc9eecbdf85bf517ff890e78a29a87e0554c
Alexey Rozhkov <rozhkov90@yandex.ru>
2016-12-12 22:59:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function onClick1() {
 		color: "#5c5c5c"
 	});
 }
+
+function onClick2() {
+	$('.js-styles').css({
+		backgroundColor: "#393939",
+		color: "#d0d0d0"
+	});
+}
+
+function onClick3() {
+	$('#js-blocks').append('<div class="js-blocks_add">'+'</div>');
+}
+
+function delValue1() {
+	$('.js-blocks_add').remove();
+}
+
+function onClick4() {
+	var first = $('.js-math_first').val();
+	var second = $('.js-math_two').val();
+	var result = first*second;
+	$('.js-math_res').val(result);
+}
