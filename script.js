7ac200d465fea1e2e561895e1667f9b5bb4e7cbe
Blazer90 <rozhkov90@yandex.ru>
2016-12-14 13:46:39 +0400

    new commit js

--- script.js
+++ script.js
@@ -3,26 +3,46 @@ function onClick1() {
 		backgroundColor: "#d0d0d0",
 		color: "#5c5c5c"
 	});
-}
+};
 
 function onClick2() {
 	$('.js-styles').css({
 		backgroundColor: "#393939",
 		color: "#d0d0d0"
 	});
-}
+};
 
 function onClick3() {
 	$('#js-blocks').append('<div class="js-blocks_add">'+'</div>');
-}
+};
 
 function delValue1() {
 	$('.js-blocks_add').remove();
-}
+};
 
-function onClick4() {
-	var first = $('.js-math_first').val();
-	var second = $('.js-math_two').val();
-	var result = first*second;
-	$('.js-math_res').val(result);
-}
+$(document).ready(function() {
+	$('.js-math_count').click(function () {
+		var first = $('.js-math_first').val() *1;
+		var second = $('.js-math_two').val() *1;
+		var result = first*second;
+		$('.js-math_res').val(result);
+	});
+	$('.js-math_count_2').click(function () {
+		var first = $('.js-math_first').val() *1;
+		var second = $('.js-math_two').val() *1;
+		var result = first/second;
+		$('.js-math_res').val(result);
+	});
+	$('.js-math_count_3').click(function () {
+		var first = $('.js-math_first').val() *1;
+		var second = $('.js-math_two').val() *1;
+		var result = first+second;
+		$('.js-math_res').val(result);
+	});
+	$('.js-math_count_4').click(function () {
+		var first = $('.js-math_first').val() *1;
+		var second = $('.js-math_two').val() *1;
+		var result = first-second;
+		$('.js-math_res').val(result);
+	});
+});
